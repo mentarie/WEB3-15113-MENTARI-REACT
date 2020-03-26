7db8fe52b774034d616566a8920b27e044c5741f
mentari <mentari.e.r@mail.ugm.ac.id>
2020-03-26 16:58:58 +0700

    Updates

---
+++ precache-manifest.1720e1068844e7098bd93fd1ca47a14b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001344930f25d778af7ee7a54c814e84",
+    "url": "/mentarie/WEB3-15113-MENTARI-REACT/index.html"
+  },
+  {
+    "revision": "238f4eda8c3832d26b56",
+    "url": "/mentarie/WEB3-15113-MENTARI-REACT/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "a92589dab9e30038b616",
+    "url": "/mentarie/WEB3-15113-MENTARI-REACT/static/css/main.3f324957.chunk.css"
+  },
+  {
+    "revision": "238f4eda8c3832d26b56",
+    "url": "/mentarie/WEB3-15113-MENTARI-REACT/static/js/2.725ce827.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/mentarie/WEB3-15113-MENTARI-REACT/static/js/2.725ce827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a92589dab9e30038b616",
+    "url": "/mentarie/WEB3-15113-MENTARI-REACT/static/js/main.e6c4f03b.chunk.js"
+  },
+  {
+    "revision": "a7721ce8cc264fe6681a",
+    "url": "/mentarie/WEB3-15113-MENTARI-REACT/static/js/runtime-main.469d51b5.js"
+  },
+  {
+    "revision": "c88992deecf30579e0d684ee21331c67",
+    "url": "/mentarie/WEB3-15113-MENTARI-REACT/static/media/picture.c88992de.jpg"
+  }
+]);
